5a94cffc264d127186b93ef7ef3f91f77407d4df
Harriet Symington <harriet.symington@hinge.co>
2025-02-19 13:10:26 -0500

    Initial Commit

---
+++ SpeedDatingScheduler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedDatingScheduler
+//
+//  Created by Harriet Symington on 2/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpeedDatingSchedulerTests/SpeedDatingSchedulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedDatingSchedulerTests.swift
+//  SpeedDatingSchedulerTests
+//
+//  Created by Harriet Symington on 2/19/25.
+//
+
+import Testing
+@testable import SpeedDatingScheduler
+
+struct SpeedDatingSchedulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
